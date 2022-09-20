53747c26a0d6026f251361de0350c2d6485fcee7
Omar Pérez <A01383853@itesm.mx>
2022-09-20 13:16:21 -0500

    PauseMenu Code and Designs
    Pause Manu fonts bugs fixed :D

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -8,8 +9,10 @@ public class PauseMenu : MonoBehaviour
     public static bool GameIsPaused = false;
 
     public GameObject pauseMenuUI;
+    public GameObject settingsUI;
+    public GameObject canvasGameUI;
 
-    void Awake() {
+    void Awake(){
         pauseMenuUI.SetActive(false);
     }
 
@@ -26,15 +29,31 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
-    void Resume(){
+    public void Resume(){
+        settingsUI.SetActive(false);
         pauseMenuUI.SetActive(false);
+        canvasGameUI.SetActive(true);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
 
+    public void Settings(){
+        settingsUI.SetActive(true);
+    }
+
     void Pause(){
         pauseMenuUI.SetActive(true);
+        canvasGameUI.SetActive(false);
         Time.timeScale = 0f;
         GameIsPaused = true;
+    }
+
+    public void LoadMenu(){
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    public void QuitGame(){
+        Application.Quit();
     }
 }
