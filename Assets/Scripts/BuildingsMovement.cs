3955fd6c50d406233676baaa0c383625ac48ab80
TerItoc <itoc210551@gmail.com>
2022-11-24 13:53:43 -0600

    Jueves de Hamsters
    Ven a Hamsburgo donde encontraras distintas tiendas, como la Forja de Sasha, Talpastle Inc y el reciente FurBar.
    (Se agregaron edificios a la escena y el movimiento entre estas zonas y el escenario principal)

---
+++ Assets/Scripts/BuildingsMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BuildingsMovement : MonoBehaviour
+{
+    public GameObject Enter;
+
+    private PlayerInputAction playerInputs;
+    private GameObject taroSale;
+    private bool swotch;
+    private Vector3 pos;
+
+    void Awake(){
+        playerInputs = new PlayerInputAction();
+    }
+
+    private void OnEnable(){
+        playerInputs.Player.Interaction.performed += teleport;
+        playerInputs.Player.Interaction.Enable();
+    }
+
+    private void OnDisable(){
+        playerInputs.Player.Interaction.Disable();
+    }
+
+    void teleport(InputAction.CallbackContext context){
+        if(swotch){
+            pos = Enter.transform.position;
+            taroSale.transform.position = pos;
+            Debug.Log(pos);
+            swotch = false;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D TuGfa){
+        if(TuGfa.gameObject.tag == "Player"){
+            taroSale = TuGfa.gameObject;
+            swotch = true;
+        }
+        else{
+            swotch = false;
+        }
+    }
+}
