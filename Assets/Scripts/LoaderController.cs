ea4bd9e6c96cd9ae3b6b7cff072ef8954234163c
Leonardo Pérez <a00829368@itesm.mx>
2022-11-24 12:48:39 -0600

    Hints in LoadScreen
    ya hay hints!

--- Assets/Scripts/LoaderController.cs
+++ Assets/Scripts/LoaderController.cs
@@ -7,13 +7,27 @@ using TMPro;
 public class LoaderController : MonoBehaviour
 {
     public TextMeshProUGUI loaderText;
+    public TextMeshProUGUI hintText;
     public float delay = 10f;
     public int maxDots = 3;
+    public string nextScene;
+
+    private string[] hints = {"El gancho de Taro no funciona en vidrio", 
+                              "Talpa tiene items interesantes en su tienda",
+                              "Sasha te ayudara a mejorar tu aguja",
+                              "Las ratas tienen mayor vida que las cucarachas",
+                              "Las bendiciones te ayudaran a aumentar el máximo de vidas",
+                              "Consume Talpas para curarte"};
 
     void Start()
     {
+        RandomHint();
         StartCoroutine(LoadingText());
         StartCoroutine(GameLoaderScene());
+    }
+
+    void RandomHint(){
+        hintText.text = hints[Random.Range(0, hints.Length)];
     }
 
     IEnumerator LoadingText(){
@@ -28,7 +42,7 @@ public class LoaderController : MonoBehaviour
 
     IEnumerator GameLoaderScene(){
         AsyncOperation op;
-        op = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("firstScene");
+        op = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(nextScene);
         op.allowSceneActivation = false;
 
         yield return new WaitForSeconds(delay);
