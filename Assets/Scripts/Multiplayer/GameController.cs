fb878d1ad9c37df0dfaa61ce6ef96d5f12b1a3fc
Omar Pérez <A01383853@itesm.mx>
2022-11-25 13:43:21 -0600

    New Multhings
    WAOS

--- Assets/Scripts/Multiplayer/GameController.cs
+++ Assets/Scripts/Multiplayer/GameController.cs
@@ -21,6 +21,8 @@ public class GameController : MonoBehaviourPunCallbacks
 
     private CoinsManager cm;
 
+    public Material secondplayer;
+
     private void Awake(){
         instance = this;
 
@@ -52,8 +54,36 @@ public class GameController : MonoBehaviourPunCallbacks
         if(playerInGame == PhotonNetwork.PlayerList.Length){
             SpawnPlayer(); //Mandar llamar posiciones de player
         }
+        
+        if(PhotonNetwork.PlayerList.Length > 1){
+            //Debug.Log(PhotonView.Find(2001).gameObject.name);
+            PhotonView.Find(2001).gameObject.transform.GetChild(0).GetChild(1).GetComponent<SkinnedMeshRenderer>().material = secondplayer;
+        }
     }
 
+    //Stuff for the CAT BOSS
+    public void preHitBoss(int damage, int viewID){
+        PhotonView photonView = PhotonView.Get(this);
+        photonView.RPC("HitBoss", RpcTarget.All, damage, viewID);
+    }
+
+    [PunRPC]
+    public void HitBoss(int damage, int viewID){
+        CatBossIA cat = PhotonView.Find(viewID).gameObject.GetComponent<CatBossIA>();
+        cat.lifes = cat.lifes - damage;
+        if(cat.lifes <= 0){
+            InventorySystem player = GameObject.FindGameObjectWithTag("Player").GetComponent<InventorySystem>();
+            player.croquetasQty = player.croquetasQty + cat.coinsToAdd;
+            player.Update_Ui(); 
+            
+            cm.coinsToAdd = cm.coinsToAdd + cat.coinsToAdd;
+            cm.addCoins();
+            PhotonNetwork.Destroy(cat.gameObject);
+        }
+        StartCoroutine(cat.DamageToEnemy(cat.transform.GetChild(0).gameObject));
+    }
+
+    //Stuff for the COMMON ENEMIES
     public void preHit(int damage, int viewID){
         PhotonView photonView = PhotonView.Get(this);
         photonView.RPC("HitEnemy", RpcTarget.All, damage, viewID);
@@ -79,11 +109,15 @@ public class GameController : MonoBehaviourPunCallbacks
     void SpawnPlayer(){
         int randomPosition = Random.Range(0, spawnPlayerPositions.Length); //Obtener una posicion random de lista de posiciones
         GameObject playerObj = PhotonNetwork.Instantiate(playerPrefab, spawnPlayerPositions[randomPosition].position, Quaternion.identity); //Instanciar el player en una posicion aleatoria
-
+        /*//playerObj.transform.GetChild(0).GetChild(1).GetComponent<SkinnedMeshRenderer>().material = secondplayer;
+        PhotonView idViewer = playerObj.GetComponent<PhotonView>();
+        Debug.Log(idViewer.ViewID);
+        if(idViewer.ViewID != PhotonNetwork.LocalPlayer.ActorNumber){
+            PhotonView.Find(idViewer.ViewID).gameObject.transform.GetChild(0).GetChild(1).GetComponent<SkinnedMeshRenderer>().material = secondplayer;
+        }*/
         
         PM playScript = playerObj.GetComponent<PM>(); //Obtener script que controla al jugador
         playScript.photonView.RPC("Init", RpcTarget.All, PhotonNetwork.LocalPlayer); //Mandar ejecutar funcion de inicializador de player
-        
     }
 
     void SpawnPlayerOffline(){
