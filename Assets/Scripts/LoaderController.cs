0b39a22a41db81bd54e0f652e6f5e66e80656406
Leonardo Pérez <a00829368@itesm.mx>
2022-11-25 20:55:06 -0600

    Alfa de LoadScene
    hay cosas por mejorar

--- Assets/Scripts/LoaderController.cs
+++ Assets/Scripts/LoaderController.cs
@@ -1,17 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
-using DG.Tweening;
 
 public class LoaderController : MonoBehaviour
 {
     public TextMeshProUGUI loaderText;
     public TextMeshProUGUI hintText;
-    public Image itemImage;
-    public Sprite[] items;
     public float delay = 10f;
     public int maxDots = 3;
     public string nextScene;
@@ -25,8 +21,6 @@ public class LoaderController : MonoBehaviour
 
     void Start()
     {
-        DOTween.Init();
-        StartCoroutine(ItemsImageMove());
         StartCoroutine(LoadingText());
         StartCoroutine(GameLoaderScene());
         RandomHint();
@@ -34,18 +28,6 @@ public class LoaderController : MonoBehaviour
 
     void RandomHint(){
         hintText.text = hints[Random.Range(0, hints.Length)];
-    }
-
-    IEnumerator ItemsImageMove(){
-        Sequence dotweenAnimation = DOTween.Sequence();
-
-        dotweenAnimation.Append(itemImage.GetComponent<Transform>().DOMove(new Vector3(750,250,0), 0f));
-        itemImage.GetComponent<Image>().sprite = items[Random.Range(0, items.Length)];
-        dotweenAnimation.Append(itemImage.GetComponent<Transform>().DOMove(new Vector3(1500,250,0), 2f));
-
-        yield return new WaitForSeconds(2f);
-
-        StartCoroutine(ItemsImageMove());
     }
 
     IEnumerator LoadingText(){
