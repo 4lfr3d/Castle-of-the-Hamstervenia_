f3dc6c1fa5a26e9394b281324aa208904839fa2c
Leonardo Pérez <a00829368@itesm.mx>
2022-11-28 13:27:07 -0600

    xd
    xd

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -8,21 +8,45 @@ using Photon.Realtime;
 public class SceneChanger : MonoBehaviourPunCallbacks
 {
     public string NextScene;
+    public GameObject taro;
+    public GameObject respawnIn;
+    public GameObject respawnOut;
 
-    public void ChangeScene(string NextScene){
+    void Awake(){
+        taro = GameObject.Find("Protag");
+        respawnIn = GameObject.Find("ChangeRespawnIn").GetComponent<Transform>();
+        respawnIn = GameObject.Find("ChangeRespawnOut").GetComponent<Transform>();
+    }
+
+    void Start(){
+        PlayerPrefs.SetString("LoadScreenNextScene", NextScene);
+        PlayerPrefs.SetFloat("LoadScreenDelay", 2f);
+    }
+
+    public void ChangeScene(string changePoint){
         if(!PhotonNetwork.IsConnected){
-            SceneManager.LoadScene(NextScene);
+            if(changePoint == "ChangePointIn"){
+                taro.GetComponent<SaveManager>().respawnPoint = respawnIn.position;
+            }
+            else{
+                taro.GetComponent<SaveManager>().respawnPoint = respawnOut.position;
+            }
+            SceneManager.LoadScene("LoadScene");
         }
         else{
+<<<<<<< Updated upstream
             MultiplayerController.instance.photonView.RPC("LoadScene", RpcTarget.All, NextScene);
             PM[] players = new PM[PhotonNetwork.PlayerList.Length];
             photonView.RPC("InGame", RpcTarget.AllBuffered);
+=======
+            MultiplayerController.instance.photonView.RPC("LoadScene", RpcTarget.All, "NextScene");
+>>>>>>> Stashed changes
         }
     }
 
     private void OnCollisionEnter2D(Collision2D other){
         if(other.collider.CompareTag("Player")){
-            ChangeScene(NextScene);
+            ChangeScene(other.gameObject.name);
         }
     }
 }
